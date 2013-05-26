a07d19dca42b4eadda9c3cf32777691837ab6767
Brian D. Burns <iosctr@gmail.com>
2013-05-25 20:58:25 -0400

    Notifier::HttpPost
    Adds a generic HTTP POST notifier.
    
    closes #371, closes #406

--- lib/backup/config.rb
+++ lib/backup/config.rb
@@ -115,7 +115,8 @@ module Backup
               { 'RSync' => ['Push', 'Pull', 'Local'] }
             ],
             # Notifiers
-            ['Mail', 'Twitter', 'Campfire', 'Prowl', 'Hipchat', 'Pushover']
+            ['Mail', 'Twitter', 'Campfire', 'Prowl',
+             'Hipchat', 'Pushover', 'HttpPost']
           ]
         )
       end
